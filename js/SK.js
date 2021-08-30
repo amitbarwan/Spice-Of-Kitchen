c1562ae3afae76ef9bb356edbbcfaf935c1bb4d4
barwan12 <amitkumar.aec05@gmail.com>
2021-08-31 03:26:51 +0530

    hideshow

--- js/SK.js
+++ js/SK.js
@@ -21,3 +21,14 @@ function displaytime() {
     document.getElementById('time').innerHTML = time;
 }
 setInterval(displaytime, 1000);
+function ShowHide() {
+
+    let pic = document.getElementById('SK');
+    // alert("Pics");
+    console.log(pic);
+    if(pic.style.display!='none')
+        pic.style.display='none';
+    else
+        pic.style.display='block'
+}
+setInterval(ShowHide, 1000);
